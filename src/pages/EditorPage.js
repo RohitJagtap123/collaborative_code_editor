3053cdeba11b538412e84bc38c7858e9afdcda60
Rohanwagh5 <rohanwagh52005@gmail.com>
2024-11-28 18:40:23 +0530

    created Routes

---
+++ src/pages/EditorPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const EditorPage = ()=>{
+
+    return (
+
+
+        <div>
+                
+            <h1>Editor page</h1>
+        
+        </div>
+    )
+
+
+}
+export default EditorPage
