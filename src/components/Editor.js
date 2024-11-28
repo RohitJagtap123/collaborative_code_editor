64ca48b63dd67a0cf16272997a6e49af72fe0fd6
Rohanwagh5 <rohanwagh52005@gmail.com>
2024-11-28 20:19:55 +0530

    Editor Page Structure  Done

---
+++ src/components/Editor.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Avatar from 'react-avatar';
+
+
+const Editor= ()=>{
+
+    return (
+
+        <div className='client'>
+
+           
+        </div>
+    )
+}
+
+export default Editor;
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Client from '../components/Client.js';
 
 const EditorPage = ()=>{
 
+    const [clients,setClients]=useState([
+        {socketId:1 ,username:'Rohan'},
+        {socketId:2 ,username:'Rohit'}
+    ]);
+
+
     return (
 
-
-        <div>
                 
-            <h1>Editor page</h1>
-        
+        <div className='Main'>
+
+            <div className='left'>
+
+                <div className='left-inner'>
+
+                    <div className='logo'>
+                         <img className='logoImg' src=''  />
+                    </div>
+
+                    <h3>Connected</h3>
+
+
+                    <div className='client-list'>
+
+                       {
+                        clients.map(client=>(
+                            <Client
+                              key={client.socketId}
+                              username={client.username}
+                            /> 
+                        ))
+                       }                    
+
+                    </div>
+                        
+                </div>
+
+
+                <button className='copybtn'>copy Room ID</button>
+                <button className='leavebtn'>Leave button</button>
+
+            </div>
+
+
+            <div className='right'>
+
+                {/* <Editor></Editor> */}
+                  
+            </div>
+
         </div>
+        
+        
     )
 
 
