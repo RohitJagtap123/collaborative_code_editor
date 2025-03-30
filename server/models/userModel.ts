21fe41a627a49b586b7de3a4ba94bbc5bab27aba
Rohan Agrawal <104892210+Rohan452004@users.noreply.github.com>
2025-03-30 23:06:49 +0530

    "Implemented OTP based SignUP and forgot password functionality"

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -5,12 +5,16 @@ export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
+  token?: string;
+  resetPasswordExpires?: Date;
 }
 
 const userSchema = new Schema<IUser>({
   name: { type: String, required: true, trim: true },
   email: { type: String, required: true, trim: true, unique: true },
   password: { type: String, required: true },
+  token: { type: String},
+  resetPasswordExpires:{ type: Date,}
 });
 
 export default mongoose.model<IUser>("User", userSchema);
